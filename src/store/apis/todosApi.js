7c4e1735953710d0e2db70a7ba846ae2cef437cc
DevRodox <rodrigo17.ed@gmail.com>
2024-08-06 12:58:24 -0600

    Change todosApi and TodoApp Add id

--- src/store/apis/todosApi.js
+++ src/store/apis/todosApi.js
@@ -14,8 +14,12 @@ export const todosApi = createApi({
     
         getTodos: builder.query({
             query: () => '/todos'
-        })
+        }),
+
+        getTodoById: builder.query({
+            query: (todoId) => `/todos/${ todoId }`
+        }),
     })
 });
 
-export const { useGetTodosQuery } = todosApi;
+export const { useGetTodosQuery, useGetTodoByIdQuery} = todosApi;
